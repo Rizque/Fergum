109a0ec0457596f7565864da17b8e09af1baab8d
Rizque <87496974+Rizque@users.noreply.github.com>
2023-04-26 11:53:46 +0300

    #

--- fergum/urls.py
+++ fergum/urls.py
@@ -17,15 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import TemplateView
 from . import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.frontpage, name='frontpage'),
-
-    path('', TemplateView.as_view(template_name="main.html")),
     path('', include('users.urls')),
     path('', include('properties.urls')),
 
--- fergum/views.py
+++ fergum/views.py
@@ -2,10 +2,5 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 
-@login_required(login_url='frontpage')
-def main(request):
-    return render(request, 'main.html')
-
-
-def home(request):
+def frontpage(request):
     return render(request, 'frontpage.html')
